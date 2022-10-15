980d0476e6109b9bc6d9625b6d757a5b972e2af3
Emir TARTAR <emirtartar35@gmail.com>
2022-10-15 12:06:38 +0300

    Add project files.

---
+++ 1- QueueSendAndReceive/Consumer/Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("mesajkuyrugu", false, false, false);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var msg = Encoding.UTF8.GetString(e.Body.Span);
+                    Console.WriteLine("Mesaj alındı: " + msg);
+                };
+
+                consumer.Registered += (sender, e) =>
+                {
+                    Console.WriteLine("Registered.");
+                };
+
+                /// autoAck : Kuruktan alınan mesajın silinip silinmemesini sağlıyor. 
+                /// Bazen kuyruktan alınan mesaj işlenirken beklenmeyen hatalarla karşılaşılabiliyor. 
+                /// O yüzden mesajı başarılı bir şekilde işlemeksizin kuyruktan silinmesini pek önermeyiz.
+                channel.BasicConsume("mesajkuyrugu", false, consumer);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1- QueueSendAndReceive/Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName= "localhost",
+            };
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                /// durable: Normal şartlarda kuyruktaki mesajların hepsi bellek üzerinde dizilirler. 
+                /// Hal böyleyken RabbitMQ sunucuları bir sebepten dolayı restart atarlarsa tüm veriler kaybolabilir. 
+                /// durable parametresine true değerini verirsek eğer verilerimiz güvenli bir şekilde sağlamlaştırılacak yani fiziksel hale getirilecektir.
+                /// exclusive: Oluşturulacak bu kuyruğa birden fazla kanalın bağlanıp bağlanmayacağını belirtir.
+                /// autoDelete: True değerine karşılık tüm mesajlar bitince kuyruğu otomaik imha eder.
+                channel.QueueDeclare("mesajkuyrugu", false, false, false);
+                byte[] msg = Encoding.UTF8.GetBytes("Havagi.");
+                /// exchange : Eğer exchange kullanmıyorsanız boş bırakınız. Böylece default exchange devreye girecek ve kullanılmış olacaktır.
+                /// routingKey : Eğer ki default exchange kullanıyorsanız routingKey olarak oluşturduğunuz kuyruğa verdiğiniz ismin birebir aynısını veriniz.
+                /// body : Gönderilecek mesajın ta kendisidir.
+                channel.BasicPublish(exchange: "", routingKey: "mesajkuyrugu", body: msg);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
